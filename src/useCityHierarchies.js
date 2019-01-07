e114f868f5223e40ba198c45d4f81c1ba6df2712
Sayan Bose <sayan@mapline.com>
2019-01-07 11:03:15 +0000

    Filtered parents on basis of placetype

--- src/useCityHierarchies.js
+++ src/useCityHierarchies.js
@@ -3,6 +3,8 @@ const logger = peliasLogger.get('wof-admin-lookup');
 
 var placeholder = require('pelias-placeholder');
 
+var allowedPlaceTypes = ['continent', 'ocean', 'empire', 'country', 'dependency', 'marinearea', 'macroregion', 'region', 'macrocounty', 'county', 'locality', 'borough', 'localadmin', 'neighbourhood', 'postalcode'];
+
 function addParentsFromMeta( doc, meta ) {
   if( doc.hasMeta(meta) ) {
     var metaValue = doc.getMeta(meta);
@@ -11,10 +13,12 @@ function addParentsFromMeta( doc, meta ) {
 	 console.log("Meta: " + meta + ", Value: " + metaValue);
 	
     	 placeholder.search( metaValue, 'locality', function( results ) {
-		console.log("Adding additional parents from city hierarchy");
-		console.log(JSON.stringify(results, null, 2));
         	results.forEach( function( result ) {
-		   if( result != null ) {
+		   if( result != null && allowedPlaceTypes.includes( result.placetype.toLowerCase() ) ) {
+
+		     console.log("Adding additional parent from city hierarchy");
+		     console.log(JSON.stringify(result, null, 2));
+
 	             doc.addParent(result.placetype, result.name, result.id.toString(), result.abbr, true);
 		   }
         	});
