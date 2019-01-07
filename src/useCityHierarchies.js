7a9fb2e563bc2374bfb2832524e1893c99d4f202
sayan <sayanb@gmail.com>
2019-01-07 10:58:37 +0000

    Adding position params

--- src/useCityHierarchies.js
+++ src/useCityHierarchies.js
@@ -3,13 +3,13 @@ const logger = peliasLogger.get('wof-admin-lookup');
 
 var placeholder = require('pelias-placeholder');
 
-function addParentsFromMeta( doc, meta ) {
+function addParentsFromMeta( doc, meta, positionParams ) {
   if( doc.hasMeta(meta) ) {
     var metaValue = doc.getMeta(meta);
     
     placeholder.search( metaValue, 'locality', function( results ) {
         results.forEach( function( result ) {
-           doc.addParent(result.placetype, result.name, result.id.toString(), result.abbr, true);
+           doc.addParent(result.placetype, result.name, result.id.toString(), result.abbr, positionParams);
         });
     });
   }
@@ -17,8 +17,8 @@ function addParentsFromMeta( doc, meta ) {
 
 function useCityHierarchies(result, doc) {
 
-  addParentsFromMeta( doc, 'cityInFileName' );
-  addParentsFromMeta( doc, 'cityInRecord' );
+  addParentsFromMeta( doc, 'cityInRecord', { inFront: true, beforeEnd: false } );
+  addParentsFromMeta( doc, 'cityInFileName', { inFront: false, beforeEnd: true });
 }  
 
 module.exports = useCityHierarchies;
