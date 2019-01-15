17b76aa3a5c8319ab86ce509a6f59a68c1556ba0
Sayan Bose <sayanb@gmail.com>
2019-01-15 14:50:10 +0530

    Added extra check

--- src/useCityHierarchies.js
+++ src/useCityHierarchies.js
@@ -32,32 +32,34 @@ function searchPlaceholder( doc, meta, positionParams ) {
   }
 }
 
-function useCityHierarchies(result, doc) {
+function useCityHierarchies(res, doc) {
 
   searchPlaceholder( doc, 'recordPlaceholderInput', { inFront: true, beforeEnd: false });
 
   if( doc.hasMeta( 'wofIdFromFileName' ) ) {
     var wofId = doc.getMeta( 'wofIdFromFileName' );
     console.log("WOFIdFromFileName " + JSON.stringify( wofId, null, 2));
-    var result = wofLookup.getWofData( wofId );
-    console.log("Result " + JSON.stringify(result, null, 2));
+    var wofAdmin = wofLookup.getWofData( wofId );
+    console.log("WofAdmin " + JSON.stringify(wofAdmin, null, 2));
 
     var params = { inFront: false, beforeEnd: true };
     
-    if( result && allowedPlaceTypes.includes( result.Placetype.toLowerCase() ) ) {
-      doc.addParent( result.Placetype, result.Name, result.Id.toString(), result.Abbrev, params );
-    }
-    
-    var parents = _.compact(result.Hierarchy[0].map(id => wofLookup.getWofData( id )));
-    console.log("Parents " + JSON.stringify(parents, null, 2));
+      if (wofAdmin && allowedPlaceTypes.includes(wofAdmin.Placetype.toLowerCase() ) ) {
+          doc.addParent(wofAdmin.Placetype, wofAdmin.Name, wofAdmin.Id.toString(), wofAdmin.Abbrev, params );
+      }
 
-    if( parents ) {
-      parents.forEach( function( p ) {
-       if( allowedPlaceTypes.includes( p.Placetype.toLowerCase() ) ) {
-         doc.addParent( p.Placetype, p.Name, p.Id.toString(), result.Abbrev, params );
-       }
-      });
-    }        
+      if (wofAdmin.Hierarchy) {
+        var parents = _.compact(wofAdmin.Hierarchy[0].map(id => wofLookup.getWofData(id)));
+        console.log("Parents " + JSON.stringify(parents, null, 2));
+
+        if (parents) {
+            parents.forEach(function (p) {
+                if (allowedPlaceTypes.includes(p.Placetype.toLowerCase())) {
+                    doc.addParent(p.Placetype, p.Name, p.Id.toString(), p.Abbrev, params);
+                }
+            });
+        }
+      }
 
   }
 }  
